f776d52857e2418fa41615ead4596ec8e3b423b1
LaiTial <39575609+LaiTial@users.noreply.github.com>
2022-07-04 10:50:37 +0900

    Update 02. severalQ.py

--- Crawling/지식in/02. severalQ.py
+++ Crawling/지식in/02. severalQ.py
@@ -16,30 +16,25 @@ soup = BeautifulSoup(html, 'html.parser')
 knowIn = {}
 nameTag = ['Question', 'Date', 'Contents', 'Link']
 i = 1
+num = 1
 
 # 지식인 list를 가져온다
-Q = soup.select_one('.basic1')
-
-#for question in Qs:
-getQs = Q.select('dt > a') #제목, 링크
-getConts = Q.select('li') # 날짜, 내용
-
+Q = soup.select('.basic1 > li')
+    
 # 제목과 링크
-for getQ, getCont in zip(getQs, getConts):
-    title = getQ.text
-    link = getQ.get('href')
-
-    tempL = [title, link]
+for lists in Q:
+        
+    #for question in Qs:
+    title = lists.select_one('a').text #제목
+    link = lists.select_one('a').attrs['href'] #링크
+        
+    date = lists.select_one('.txt_inline').text # 날짜
+    content = lists.select_one('.txt_inline+dd').text # 내용 #dd:nth-of-type(2) dd:nth-of-child(3) 가능
     
-    content = getCont.select('dd') # 날짜, 내용
-    
-    # 날짜와 본문내용을 얻어온다
-    tempL.insert(1, content[0].text)
-    tempL.insert(2, content[1].text)
-
-    knowIn["Q"+str(i)] = tempL
-    
-    i += 1
+    tempL = [title, date, content, link]
+    knowIn["Q"+str(num)] = tempL
+        
+    num += 1
 
 for title, contents in knowIn.items():
     print(f"[{title}]\n")
