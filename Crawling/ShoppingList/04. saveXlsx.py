1651f4f52e4a8d9a7d3c11de39c78c0e27add964
LaiTial <39575609+LaiTial@users.noreply.github.com>
2022-07-05 10:09:54 +0900

    Update 04. saveXlsx.py

--- Crawling/ShoppingList/04. saveXlsx.py
+++ Crawling/ShoppingList/04. saveXlsx.py
@@ -9,6 +9,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys #드라이버가 특정 키를 입력하게 만들때 사용
 import time
+from bs4 import BeautifulSoup
 import openpyxl
 
 def makeXlsx(name, tagData):
@@ -19,12 +20,12 @@ def makeXlsx(name, tagData):
     
     # 데이터 추가
     ws['A1'] = 'Num'
-    ws['B1'], ws['C1'], ws['D1'] = tagData
+    ws['B1'], ws['C1'], ws['D1'], ws['E1'] = tagData
     
     return wb, ws
 
 targetsite = "https://search.shopping.naver.com/search/all?query="
-tagData = ['product', 'price', 'link']
+tagData = ['product', 'price', 'img', 'link']
 keyword = input("keyword? ")
 
 wb, ws = makeXlsx(keyword, tagData)
@@ -40,16 +41,29 @@ driver.implicitly_wait(20) #드라이버 구동 후 n초 동안 기다린다.
 driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.END) # 맨 밑까지 스크롤을 내린다.
 time.sleep(1) #잠깐의 텀을 둔다
 
-# 컨테이너
-infos = driver.find_elements(By.CSS_SELECTOR, "li.basicList_item__2XT81")
+# 페이지 소스를 얻어온다.
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
 
-for i, info in enumerate(infos):
-    productN = info.find_element(By.CSS_SELECTOR, '.basicList_link__1MaTN').text.strip() #상품명
-    link = info.find_element(By.CSS_SELECTOR, '.basicList_link__1MaTN').get_attribute('href') #상세페이지 링크
-    price = info.find_element(By.CSS_SELECTOR, '.price_num__2WUXn').text.strip() #가격
+# 컨테이너
+contentBox = soup.select('.basicList_item__2XT81')
+
+print("\n")
+
+for i, info in enumerate(contentBox):
     
-    ws.append([i, productN, price, link])
+    product = info.select_one('.basicList_link__1MaTN') #a 태그 가져온다
+    
+    productN = product.text.strip() #상품명
+    link = product.attrs['href'] #상세페이지 링크
+    price = info.select_one('.price_num__2WUXn').text.strip() #가격
+    
+    try:
+        img = info.select_one('.thumbnail_thumb__3Agq6 > img').attrs['src'] #이미지 경로 가져온다
+    except:
+        continue;
+        
+    ws.append([i, productN, price, img, link])
     
 wb.save("shopping.xlsx")
-
     
